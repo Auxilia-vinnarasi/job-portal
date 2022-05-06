dcecc7002ab9cd94c8cc151e8a846c1f8f8d6daa
Auxilia Vinnarasi <auxiliavinnarasi@gmail.com>
2022-05-06 17:24:55 +0530

    commit

--- frontend/src/lib/apiList.js
+++ frontend/src/lib/apiList.js
@@ -1,4 +1,4 @@
-export const server = "https://job-searchingapp.herokuapp.com/";
+export const server = "https://job-searchingapp.herokuapp.com";
 
 const apiList = {
   login: `${server}/auth/login`,
